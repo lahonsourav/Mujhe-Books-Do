3646c6052670e1c5216a92198655806c993b6014
Sourav <lahoncs@gmail.com>
2023-01-01 23:29:19 +0530

    sign in and register with email

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import "./Login.css";
 import LOGU from "../../images/booklogo.png";
 
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -11,11 +16,8 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
-
   const [user, loading] = useAuthState(auth);
-
   const googleProvider = new GoogleAuthProvider();
-
   const GoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
@@ -25,6 +27,9 @@ const Login = () => {
     }
   };
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   useEffect(() => {
     if (user) {
       navigate("/");
@@ -32,6 +37,34 @@ const Login = () => {
       console.log("login");
     }
   }, [user]);
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        // Signed in
+        console.log(auth); // ...
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // ..
+      });
+  };
+
+  // from here
+  const register = (e) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((auth) => {
+        // Signed in
+        console.log(auth); // ...
+      })
+      .catch((error) => {
+        console.log(error.message);
+        // ..
+      });
+  };
+  // to here
 
   return (
     <div className="login">
@@ -43,16 +76,32 @@ const Login = () => {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button type="submit" className="login__signInButton">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
             Sign In
           </button>
         </form>
         <p>By signing in you agree to our privacy policy</p>
         <button onClick={GoogleLogin} className="login__registerButton">
           Sign in with Google
+        </button>
+
+        <button onClick={register} className="login__registerButton">
+          Sign Up
         </button>
       </div>
     </div>
--- src/components/utils/firebase.js
+++ src/components/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // const firebaseConfig = {
 //   apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -24,4 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+const auth = getAuth();
+const db = getFirestore();
+
+export { db, auth };
